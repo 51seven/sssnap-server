3e99d75e7c9d69305c47a360039574c8306f3ff8
Timo Mämecke <timo@maemecke.com>
2015-04-26 04:38:25 +0200

    Passport Authorization and Registration

--- config/values.js
+++ config/values.js
@@ -1,3 +1,3 @@
 module.exports = {
-  baseUrl = "https://localhost:4000/"
+  baseUrl: "https://localhost:4000/"
 }
--- launch.sample.sh
+++ launch.sample.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
+for i in "$@"
+do
+  IFS="=" read -a array <<< "${i}"
+  key="${array[0]}"
+  value="${array[1]}"
+
+case $key in
+  PORT)
+  SETPORT="$value"
+  shift
+  ;;
+  NODE_ENV)
+  SETENVIRONMENT="$value"
+  shift
+  ;;
+  *)
+    # unknown option
+  ;;
+esac
+shift
+done
+
 GOOGLE_CLIENT_ID=INSERT_YOUR_CLIENT_ID.apps.googleusercontent.com \
-GOOGLE_CLIENT_KEY=INSERT_YOUR_CLIENT_SECRET \
-NODE_ENV=$1 \
+GOOGLE_CLIENT_SECRET=INSERT_YOUR_CLIENT_SECRET \
+NODE_ENV=$SETENVIRONMENT \
+PORT=$SETPORT \
 node server.js;
